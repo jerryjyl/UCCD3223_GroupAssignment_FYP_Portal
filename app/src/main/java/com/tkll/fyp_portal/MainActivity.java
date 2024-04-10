bb6db9323ecbbd6acc735f8754fb07d2d258e1b6
jerryjyl <liongjunyong2002@gmail.com>
2024-04-10 18:13:01 +0800

    Main page complete
    Completed a simple mark up of homepage, next will work on drawer function for quick access toolbar

--- app/src/main/java/com/tkll/fyp_portal/MainActivity.java
+++ app/src/main/java/com/tkll/fyp_portal/MainActivity.java
@@ -1,6 +1,11 @@
 package com.tkll.fyp_portal;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +19,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView introTextView = findViewById(R.id.textViewIntro);
+        TextView introCollapseButton = findViewById(R.id.introCollapseBtn);
+        WebView announcementContentWv= findViewById(R.id.webViewAnnouncementsContents);
+        ImageView announcementDropdownTg = findViewById(R.id.announcementDropdownToggle);
+
+        announcementContentWv.loadUrl("file:///android_asset/announcements.html");
+
+        introCollapseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (introTextView.getVisibility() == View.GONE) {
+                    introTextView.setVisibility(View.VISIBLE);
+                    introCollapseButton.setText("Collapse");
+                } else {
+                    introTextView.setVisibility(View.GONE);
+                    introCollapseButton.setText("Expand");
+                }
+            }
+        });
+
+        announcementDropdownTg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (announcementContentWv.getVisibility() == View.GONE) {
+                    announcementContentWv.setVisibility(View.VISIBLE);
+                    announcementDropdownTg.setImageResource(R.drawable.ic_arrow_drop_down);
+                } else {
+                    announcementContentWv.setVisibility(View.GONE);
+                    announcementDropdownTg.setImageResource(R.drawable.ic_arrow_drop_up);
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 }
