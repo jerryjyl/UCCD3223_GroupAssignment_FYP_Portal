39add19f8d1e4dd8d655debdcaa8685df3732854
jerryjyl <liongjunyong2002@gmail.com>
2024-04-11 12:33:59 +0800

    DrawerMenu update (incomplete)
    Worked on DrawerMenu, still not completed yet

--- app/src/main/java/com/tkll/fyp_portal/MainActivity.java
+++ app/src/main/java/com/tkll/fyp_portal/MainActivity.java
@@ -1,25 +1,66 @@
 package com.tkll.fyp_portal;
 
-import android.media.Image;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebView;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView nvgView = findViewById(R.id.navigation_view);
+        nvgView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle drawerTg = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_navigation, R.string.close_navigation);
+
+        drawerLayout.addDrawerListener(drawerTg);
+        drawerTg.syncState();
+
+        if (savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainPage()).commit();
+            nvgView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch(menuItem.getItemId()) {
+            case (R.id.nav_home):
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainPage()).commit();
+                break;
+
+//            case R.id.nav_home:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainPage()).commit();
+//                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else super.onBackPressed();
     }
 }
