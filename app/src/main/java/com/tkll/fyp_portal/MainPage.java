0623e8ae8a2f980d60b50043386513c1729e1b3b
ShengJiet <leeshengjiet@1utar.my>
2024-04-20 15:56:59 +0800

    我做到了

--- app/src/main/java/com/tkll/fyp_portal/MainPage.java
+++ app/src/main/java/com/tkll/fyp_portal/MainPage.java
@@ -9,13 +9,52 @@ import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainPage extends Fragment {
+    private TextView usernameTextView;
+    private DatabaseReference databaseReference;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_main_page, container, false);
+
+        usernameTextView = rootView.findViewById(R.id.textView2);
+
+        // Initialize Firebase components
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser != null) {
+            String userEmail = currentUser.getEmail();
+            DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+            Query query = usersRef.orderByChild("Email").equalTo(userEmail);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                        String username = userSnapshot.child("Username").getValue(String.class);
+                        if (username != null) {
+                            usernameTextView.setText(username);
+                            break; // Assuming there's only one user with the same email
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Handle errors
+                }
+            });
+        }
 
         TextView introTextView = rootView.findViewById(R.id.textViewIntro);
         TextView introCollapseButton = rootView.findViewById(R.id.introCollapseBtn);
